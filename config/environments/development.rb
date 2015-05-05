d89d81a81249cbd64827286b3e706ebe64efb9c3
Adam Debbagh <adamdebbagh@gmail.com>
2015-05-05 11:17:12 -0400

    Add devise users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # config needed for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "jobboardbox@gmail.com",
+      password: "testjobboard"
+
+  }
+
 end
